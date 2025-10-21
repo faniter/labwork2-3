1098368efd28f34b5b52f1795f35714b77e3015f
mikolka <miklola318@gmail.com>
2025-10-21 15:17:06 +0300

    change app.py

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
+# 1. Імпортуємо SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# 2. Налаштовуємо базу даних
+# Вказуємо, що наша БД буде називатись 'shop.db' і лежатиме в тій самій папці
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+# 3. Створюємо "Модель" (опис таблиці)
+# Це наш "креслення" для кросівок
+class Sneaker(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # Унікальний ID
+    name = db.Column(db.String(100), nullable=False) # Назва
+    description = db.Column(db.Text, nullable=True) # Опис
+    price = db.Column(db.Integer, nullable=False) # Ціна
+    image_url = db.Column(db.String(200), nullable=True) # Посилання на фото
+
+    def __repr__(self):
+        return f'<Sneaker {self.name}>'
+
+# 4. Оновлюємо маршрут (route), щоб він брав дані з БД
 @app.route('/')
 def home():
-    return render_template('home.html')
+    # Замість простого рендерингу, ми спочатку
+    # просимо у БД *всі* кросівки, які в ній є
+    all_sneakers = Sneaker.query.all()
+    
+    # Потім передаємо цей список у наш шаблон
+    return render_template('home.html', sneakers=all_sneakers)
 
 @app.route('/about')
 def about():
